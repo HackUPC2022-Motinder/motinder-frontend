c51c93e37d609e0f4b5b6652bb4fdbf75e7bf29a
19IvanL <19lopezivan@gmail.com>
2022-05-01 02:57:47 +0200

    Defined Motorbike object

--- lib/controllers/motorbike.dart
+++ lib/controllers/motorbike.dart
@@ -1,1 +1,39 @@
-class Motorbike {}
+class Motorbike {
+  final String mId;
+  final String mBrand;
+  final String mModel;
+  final String mVersion;
+  final String mYear;
+  final String mKm;
+  final String mSellPrice;
+  final String mPurchasePrice;
+
+  const Motorbike({
+    required this.mId,
+    required this.mBrand,
+    required this.mModel,
+    required this.mVersion,
+    required this.mYear,
+    required this.mKm,
+    required this.mSellPrice,
+    required this.mPurchasePrice,
+  });
+
+  showMotorbikeInfo() {
+    print("Motorbike ${mId}: ${mBrand} ${mModel} (${mVersion})");
+    print("Year: ${mYear} | Km: ${mKm}");
+    print("Sell price: ${mSellPrice} | Purchase price: ${mPurchasePrice}");
+  }
+
+  factory Motorbike.fromJson(Map<String, dynamic> json) {
+    return Motorbike(
+        mId: json['id'],
+        mBrand: json['brand'],
+        mModel: json['model'],
+        mVersion: json['version'],
+        mYear: json['year'],
+        mKm: json['km'],
+        mSellPrice: json['sell_price'],
+        mPurchasePrice: json['purchase_price']);
+  }
+}
--- lib/controllers/motorbike_controller.dart
+++ lib/controllers/motorbike_controller.dart
@@ -1,12 +1,19 @@
-class MotorbikeController {
-  /*Future<MotorBike?> getMotorbike() async {
-    var client = http.Client();
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:share_it/controllers/motorbike.dart';
 
-    var uri = Uri.parse('https://');
-    var response = await client.get(uri);
+class MotorbikeController {
+  static Future<Motorbike> fetchRandomMotorbike() async {
+    final response = await http.get(
+        Uri.parse('https://motinder-api.herokuapp.com/getRandomMotorbike'));
     if (response.statusCode == 200) {
-      var json = response.body;
-      // return postFromJson(json);
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Motorbike.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load motorbike');
     }
-  }*/
+  }
 }
